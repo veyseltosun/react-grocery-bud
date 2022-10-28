2fc24383b554eda47731a282f474215ef32cc445
veyseltosun <postalarim1923@gmail.com>
2022-10-28 21:58:39 +0300

    list js

--- grocery/src/List.js
+++ grocery/src/List.js
@@ -1,4 +1,33 @@
-import React from 'react'
+import React from 'react';
+import {FaEdit, FaTrash} from "react-icons/fa";
+const List = ({items, removeItem, editItem}) => {
+  return(
+    <div className='grocery-list'>
+      {items.map((item)=>{
+        const{id, title} = item;
+        return(
+          <article className='grocery-item' key={id}>
+            <p className='title'>{title}</p>
+            <div className='btn-container'>
+              <button 
+              type='button'
+              className='edit-btn'
+              onClick={()=> editItem(id)}>
+                <FaEdit/>
+              </button>
+              <button
+               type='button'
+               className='delete-btn'
+               onClick={() => removeItem(id)}>
+                <FaTrash></FaTrash>
+               </button>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  )
+}
 
 function List() {
   return (
