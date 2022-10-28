58aad55b341055e3550c9b8111ca47b9ed17ae20
veyseltosun <postalarim1923@gmail.com>
2022-10-28 21:48:31 +0300

    alert js

--- grocery/src/Alert.js
+++ grocery/src/Alert.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {useEffect} from "react";
 
-function Alert() {
+
+function Alert({type, msg, removeAlert, list}) {
+  useEffect(() => {
+    const timeout = setTimeout(() =>{
+      removeAlert();
+    }, 3000);
+    return() => clearTimeout(timeout);
+  }[list]);
+
   return (
-    <div>Alert</div>
+   <div><p className={`alert alert-${type}`}>{msg}</p></div>
   )
 }
 
