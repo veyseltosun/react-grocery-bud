983f86c8fc3686c2bcf233c63f2763909404c6bd
veyseltosun <postalarim1923@gmail.com>
2022-11-12 02:17:26 +0300

    fav icon

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -88,7 +88,7 @@ useEffect(()=>{
     <section className='section-center'>
       <form className="grocery-form" onSubmit={handleSubmit}>
         {alert.show && <Alert {...alert} removeAlert={showAlert} list={list}/>}
-        <h3>market listem</h3>
+        <h3>veysel'in market listesi</h3>
         <div className='form-control'>
           <input type="text" className='grocery' placeholder='e.g. eggs' value={name} onChange={(e)=>setName(e.target.value)}></input>
           <button type="submit" className='submit-btn'>{isEditing ? "düzelt" : "ekle"}</button>
