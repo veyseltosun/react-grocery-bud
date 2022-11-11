faf19318b1fe93cbd01b8dde9e20db640bb83780
veyseltosun <postalarim1923@gmail.com>
2022-11-11 18:40:28 +0300

    fırst commıt after long era

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -25,54 +25,10 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({show:false, msg: "", type: ""});
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(!name){
-      showAlert(true, "danger", "please enter value");
-
-    } else if (name && isEditing){
-      setList(
-        list.map((item) =>{
-          if(item.id === editID){
-            return{...item, title: name};
-
-          }
-          return item;
-        })
-
-      )
-      setName("");
-      setEditID (null);
-      setIsEditing(false);
-      showAlert(true, "success", "value changed");
-
-    } else{
-      showAlert(true, "success", "item added to the list");
-      const newItem = {id: new Date().getTime().toString(), title:name};
-
-      setList([...list, newItem]);
-      setName("");
-
-    }
-  }
-
-  const showAlert = (show = false, type = "", msg = "") => {
-
-    setAlert({show, type, msg});
-
-  }
-  const clearList = () =>{
-    showAlert(true, "danger", "empty list");
-    setList([]);
-
-  }
-  const removeItem = (id) => {
-    showAlert(true, "danger", "item removed");
-    setLis
-  }
-  return (
-    <div>Grocery Bud App</div>
+  return(
+    <section className='section-center'>
+      
+    </section>
   )
 }
 
